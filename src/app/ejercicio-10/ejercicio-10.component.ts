28cb818d947c2c62087192ea0afeb6e27850daea
Karen Lizbeth Negrete Hernández <230570@utxicotepec.edu.mx>
2025-03-06 00:00:18 -0600

    Agregación
     se agregaron los ejercicios del tutorial de angular

---
+++ src/app/ejercicio-10/ejercicio-10.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio-10',
+  imports: [],
+  templateUrl: './ejercicio-10.component.html',
+  styleUrl: './ejercicio-10.component.css'
+})
+export class Ejercicio10Component {
+
+}
